0ecc09276143cf87a67ec4a1030184dd00555b0d
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-04-11 19:18:07 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import PigTile from "./PigTile";
+
+
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<PigTile hogs={hogs}/>
 		</div>
 	);
 }
